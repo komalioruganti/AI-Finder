de356a9f7941ebf67db1b9476be867e267ac0b70
Chinmaya Kumar Behera <official.chinu2127@gmail.com>
2023-05-02 08:46:00 +0530

    changes in cross origin issue

--- service/index.js
+++ service/index.js
@@ -5,7 +5,12 @@ const cors = require("cors");
 const connectDB = require("./db");
 
 connectDB();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://main--elaborate-eclair-6eb7bd.netlify.app",
+    credentials: true,
+  })
+);
 app.use("/api", router);
 
 app.listen(5000, () => {
